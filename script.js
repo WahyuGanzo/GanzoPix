ca9c734d7c33843338683de47cb32fc6311bf461
Wahyu Ganzo <85389064+WahyuGanzo@users.noreply.github.com>
2025-01-04 21:19:21 +0700

    Update

--- script.js
+++ script.js
@@ -17,6 +17,26 @@ const categories = {
             { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735990282/472106910_1006569298179242_318721468635925547_n_pthcms.jpg", downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735990282/472106910_1006569298179242_318721468635925547_n_pthcms.jpg" },
             { url: "asset/klikdisini.jpg", downloadLink: "spongebobsquarepants/spongebobsquarepants.html" }
         ]
+    },
+    'cutscene': {
+        title: "SpongeBob Squarepants Cut Scene",
+        images: [
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469022487_542140278632351_6796819381694030705_n_kqseal.jpg", downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469022487_542140278632351_6796819381694030705_n_kqseal.jpg" },
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469150539_543580511821661_7040675705189656914_n_lvkd4s.jpg", downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469150539_543580511821661_7040675705189656914_n_lvkd4s.jpg" },
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469199601_543580478488331_419413395457256379_n_mp5vol.jpg", downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469199601_543580478488331_419413395457256379_n_mp5vol.jpg" },
+            { url: "asset/klikdisini.jpg", downloadLink: "spongebobsquarepants/cutscene/cutscene.html" }
+        ]
+    },
+    'tools': {
+        title: "Tools Yang Kami Pakai Dalam Pembuatan Website Ini",
+        images: [
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1736000054/html_uki1ne.png", downloadLink: "https://id.wikipedia.org/wiki/HTML" },
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735999882/css-3_uvvj2n.png", downloadLink: "https://id.wikipedia.org/wiki/Cascading_Style_Sheets" },
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735999884/javascript-39400_owumk1.png", downloadLink: "https://id.wikipedia.org/wiki/JavaScript" },
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735999884/Visual_Studio_Code_1.35_icon.svg_ldu3b2.png", downloadLink: "https://code.visualstudio.com/" },
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735999883/cloudinary-icon_rz3o6d.png", downloadLink: "https://cloudinary.com/" },
+            { url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735999881/brave-browser-icon_prpzdq.png", downloadLink: "https://brave.com/id/" }
+        ]
     }
 };
 
---
+++ spongebobsquarepants/cutscene/cutscene.js
@@ -0,0 +1,96 @@
+// Image Data - Upin Ipin
+const images = [
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469022487_542140278632351_6796819381694030705_n_kqseal.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469022487_542140278632351_6796819381694030705_n_kqseal.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469150539_543580511821661_7040675705189656914_n_lvkd4s.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469150539_543580511821661_7040675705189656914_n_lvkd4s.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469199601_543580478488331_419413395457256379_n_mp5vol.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469199601_543580478488331_419413395457256379_n_mp5vol.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469292794_543580471821665_732349672746405157_n_oht8vv.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469292794_543580471821665_732349672746405157_n_oht8vv.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469278805_543580458488333_5108685267429452156_n_uviqq2.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469278805_543580458488333_5108685267429452156_n_uviqq2.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469045226_542140328632346_8312027200863130494_n_ycskje.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469045226_542140328632346_8312027200863130494_n_ycskje.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469212364_543580461821666_667696995903345629_n_ibtdor.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469212364_543580461821666_667696995903345629_n_ibtdor.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469421519_543580451821667_1467719352784611508_n_q0hbci.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998897/469421519_543580451821667_1467719352784611508_n_q0hbci.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/468928618_542140178632361_2505444324755634987_n_rtdpja.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/468928618_542140178632361_2505444324755634987_n_rtdpja.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/468879282_542140218632357_4711624569473896712_n_xiucfl.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/468879282_542140218632357_4711624569473896712_n_xiucfl.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469122486_542140208632358_7467550074455152387_n_flsdvn.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998896/469122486_542140208632358_7467550074455152387_n_flsdvn.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/469077765_543580455155000_1849782634793020908_n_grdlw2.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/469077765_543580455155000_1849782634793020908_n_grdlw2.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/468872518_542140228632356_6697562234624833778_n_pr2lox.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/468872518_542140228632356_6697562234624833778_n_pr2lox.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/469088689_543580448488334_1128677214175357416_n_wo6wot.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/469088689_543580448488334_1128677214175357416_n_wo6wot.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/468851396_542140268632352_6048016320754473462_n_t3jxza.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/468851396_542140268632352_6048016320754473462_n_t3jxza.jpg"
+    },
+    {
+        url: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/468783071_542140161965696_8785648819842445632_n_bqb5ga.jpg", 
+        downloadLink: "https://res.cloudinary.com/dynq3ybyz/image/upload/v1735998895/468783071_542140161965696_8785648819842445632_n_bqb5ga.jpg"
+    },
+    {
+        url: "../../asset/lagiupload.jpg", 
+        downloadLink: ""
+    },
+];
+
+
+
+// Function to create image cards - Spongebob Squarepants
+function createImageCards() {
+    const gallery = document.getElementById('cutscene');
+    
+    images.forEach(image => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const img = document.createElement('img');
+        img.src = image.url;
+        img.alt = 'Image';
+        img.addEventListener('click', () => {
+            window.location.href = image.downloadLink;
+        });
+
+        card.appendChild(img);
+        gallery.appendChild(card);
+    });
+}
+
+// Initialize gallery on page load
+window.onload = createImageCards;
